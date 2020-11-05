fd99c508f175238e3cab8cf7c3ba33cc24c92f5b
NinjaSurge <rayktd@gmail.com>
2020-11-05 11:03:52 -0600

    changes

--- Dungeon/src/com/SurgeIndustries/DungeonCrawler/Launcher.java
+++ Dungeon/src/com/SurgeIndustries/DungeonCrawler/Launcher.java
@@ -3,7 +3,8 @@ package com.SurgeIndustries.DungeonCrawler;
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hey There!");
+		System.out.println("Hi!");
 
 	}
 
