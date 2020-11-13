2d59300c7e034c89e7ea83ccea5d1c52db370d30
NinjaSurge <rayktd@gmail.com>
2020-11-12 18:15:35 -0600

    Starting

---
+++ Dungeon/src/com/SurgeIndustries/DungeonCrawler/Application.java
@@ -0,0 +1,30 @@
+package com.SurgeIndustries.DungeonCrawler;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Application extends JFrame {
+
+	public Application() {
+		
+		initUI();
+	}
+	
+	public void initUI () {
+		
+		add(new Game());
+		
+		setSize(250, 200);
+		setTitle("Application");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(() -> {
+				Application ex = new Application();
+				ex.setVisible(true);
+		});
+	}
+}
---
+++ Dungeon/src/com/SurgeIndustries/DungeonCrawler/Game.java
@@ -0,0 +1,8 @@
+package com.SurgeIndustries.DungeonCrawler;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel {
+
+	public Game() {}
+}
